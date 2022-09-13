ad464c055858a2c1a639eada06cef91430e2f19d
Eduardo Vega <eduardovr1999@gmail.com>
2022-09-13 19:48:12 +0100

    Commit

--- controller/pager.controller.ts
+++ controller/pager.controller.ts
@@ -9,9 +9,12 @@ export const sendMessage = async (req: Request, res: Response) => {
 
     const { ip, port, message, system, pager } = req.body;
 
-    const data1 = `<Login services=\"NetPage:2.1;Heartbeat\"/>\r\n`;
-    const data2 = `<PageRequest><Pager Type="Text" SystemID="${system}" ID="${pager}"/><Message>${message}\\fFVB</Message></PageRequest>\r\n`;
+    // const data1 = `<Login services=\"NetPage:2.1;Heartbeat\"/>\r\n`;
+    // const data2 = `<PageRequest><Pager Type="Text" SystemID="${system}" ID="${pager}"/><Message>${message}\\fFVB</Message></PageRequest>\r\n`;
 
+
+    const data1 = `<Login services="NetPage" />`;
+    const data2 = `<PageRequest pager="2;1" message="Flash5Min" />`
     
     client.connect({ host: ip, port: Number(port) }, async () => {
         client.write(data1);
